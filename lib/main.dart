8146b5d0f97e9ea625941b4acd54b976880348ac
Neta4302 <54069403+Neta4302@users.noreply.github.com>
2023-09-18 15:05:14 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+//import 'package:mobile_application/screens/onboarding_screen.dart';
+import 'package:mobile_application/signIn/sign_in.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ON BOARDING',
+      theme: ThemeData.light(useMaterial3: true),
+      home: SignInScreen()
+    );
+  }
+}
